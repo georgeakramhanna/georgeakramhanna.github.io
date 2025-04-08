f04c9b80bb69ce0e82daab19f8784c4088d71d5d
George Akram Hanna <ga1534@fayoum.edu.eg>
2025-04-08 18:09:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="ar">
+<?php
+     include('layouts/header.php');
+?>
+   <body class="bg-orange-100">
+
+   <?php
+     include('layouts/navbar.php');
+   ?>
+
+      <!-- Main Content -->
+      <div class="container mx-auto p-4">
+         <div class="mr flex flex-col md:flex-row gap-4">
+            <!-- Content Section -->
+            <div class="md:w-2/3 content-section">
+               <div class="mt-4 bg-white p-4 shadow-2xl rounded-lg">
+                  <p class="p-ar mb-4 text-lg text-gray-700 leading-8">
+                     أنا مبرمج ومصمم مواقع متخصص في تطوير الويب وتصميم التجربة البصرية. أتمتع بشغف كبير في إنشاء مشاريع ضخمة ومبتكرة تعكس الجمال والابتكار في كل تفصيل. أسعى دائمًا لتقديم حلول تقنية متكاملة تجمع بين الأداء العالي والتصميم الجذاب ، مما يجعل كل مشروع أقوم به تحفة فنية وتقنية.
+
+                     لقد عملت على تنفيذ مشاريع كبيرة تنوعت بين تطوير مواقع ويب حديثة، وتصميم أنظمة مخصصة، وبناء مشاريع مبتكرة تلبي احتياجات العملاء بأفضل طريقة ممكنة. أحرص دائمًا على تقديم جودة استثنائية مع أسعار تنافسية ، مع الالتزام التام بـ المواعيد المحددة لإنجاز المشاريع.
+                     
+                     إذا كنت تبحث عن شريك تقني يمكنه تحويل أفكارك إلى واقع رقمي مذهل، فأنا هنا لمساعدتك في تحقيق ذلك بأسلوب يجمع بين الإبداع والاحترافية .
+                  </p>
+               </div>
+               <div class="mt-9 bg-white p-4 shadow-2xl rounded-lg">
+
+                  <div class="flex flex-col items-end">
+                     <h1 class="text-2xl font-bold text-orange-600 mb-4">مهاراتي</h1>
+                     <hr class="border-orange-500 border-2 w-14 rounded-full">
+                   </div>
+
+
+                  <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                     <div class="">
+                        <h1 class="py-4 text-gray-800 font-semibold">Programming Languages</h1>
+                        <h1 class="py-4 text-gray-800 font-semibold">Frameworks & Libraries</h1>
+                        <h1 class="py-4 text-gray-800 font-semibold">Version Control</h1>
+                        <h1 class="py-4 text-gray-800 font-semibold">Databases</h1>
+                        <h1 class="py-4 text-gray-800 font-semibold">Asynchronous Programming</h1>
+                        <h1 class="py-4 text-gray-800 font-semibold">Design Tools</h1>
+                        <h1 class="py-4 text-gray-800 font-semibold">Architectural Patterns</h1>
+                      </div>
+                      <div>
+                        <h1 class="py-4 text-gray-800">HTML, CSS,  JavaScript, PHP, SQL</h1>
+                        <h1 class="py-4 text-gray-800">Laravel, Bootstrap, Tailwind CSS, jQuery</h1>
+                        <h1 class="py-4 text-gray-800">Git, GitHub</h1>
+                        <h1 class="py-4 text-gray-800">MySQL</h1>
+                        <h1 class="py-4 text-gray-800">AJAX</h1>
+                        <h1 class="py-4 text-gray-800">Figma</h1>
+                        <h1 class="py-4 text-gray-800">MVC</h1>
+                      </div>
+                  </div>
+
+
+               </div>
+
+
+               <div class="mt-9 bg-white p-4 shadow-2xl rounded-lg">
+
+                  <div class="flex flex-col items-end">
+                     <h1 class="text-2xl font-bold text-orange-600 mb-4">خدماتى</h1>
+                     <hr class="border-orange-500 border-2 w-14 rounded-full">
+                   </div>
+
+
+                  <div class="p-ar mt-7 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                     <div class="bg-orange-100 shadow-xl p-4 rounded-lg">
+                        <div class="flex rounded-lg justify-right gap-4">
+                           <img src="https://cdn-icons-png.flaticon.com/512/17134/17134159.png" class="image-icon">
+                           <h1 class="text-orange-600 sm:text-lg text-xl mb-3 leading-9 font-semibold">تصميم وبرمجة صفحات الهبوط احترافية</h1>
+                        </div>
+                        <p class="leading-8">
+                           اقدم تصميمًا وبرمجة احترافية لصفحات الهبوط التي تجمع بين الجمال البصري والأداء العالي. وتصميم متجاوب يتناسب مع جميع الأجهزة. وتركيز على تحسين تجربة المستخدم (UX) لزيادة التفاعل.</p>
+                     </div>
+                     <div class="bg-orange-100 shadow-xl p-4 rounded-lg">
+                        <div class="flex rounded-lg justify-right gap-4">
+                           <img src="https://cdn-icons-png.flaticon.com/512/11972/11972599.png" class="image-icon-2">
+                           <h1 class="text-orange-600 sm:text-lg text-xl mb-3 leading-9 font-semibold">تطوير أنظمة رقمية متكاملة</h1>
+                        </div>
+                        <p class="leading-8">بساعدك في بناء أنظمة مخصصة تناسب أفكارك وأهدافك، سواء كانت منصة تعليمية او متجر الكترونى أو اى فكرة أخرى. وايضا تصميم وتطوير أنظمة إدارة مخصصة مثل إدارة العملاء (CRM) أو إدارة المخزون.</p>
+                     </div>
+                     <div class="bg-orange-100 shadow-xl p-4 rounded-lg">
+                        <div class="flex rounded-lg justify-right gap-4">
+                           <img src="https://cdn-icons-png.flaticon.com/512/11972/11972374.png" class="image-icon-3">
+                           <h1 class="text-orange-600 sm:text-lg text-xl mb-3 leading-9 font-semibold">تعديل وصيانة مشاريع لارافيل</h1>
+                        </div>
+                        <p class="leading-8">
+                           إذا كنت تملك مشروعًا مبنيًا على لارافيل يحتاج إلى تحديث أو تحسين، انا هنا للمساعدة. لاضافة ميزات جديدة وتحسين الأداء وإصلاح الأخطاء.
+                        </p>
+                     </div>
+                  </div>
+
+
+               </div>
+
+
+
+
+            </div>
+            <!-- Profile Section -->
+            <?php
+             include('layouts/profile.php');
+            ?>
+         </div>
+      </div>
+
+    <?php
+     include('layouts/footer.php');
+    ?>
+   </body>
+</html>
